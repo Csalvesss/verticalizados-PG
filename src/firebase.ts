ffb1be315520b8e8ba20f83944fd7506ceb68392
Csalvesss <ads.cesaralves@gmail.com>
2026-03-06 13:47:42 -0300

    firebase env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBIhT0pjEza4bxAsl7fWg19VJFHvKqKzdk',
-  authDomain: 'verticalizados-pg.firebaseapp.com',
-  projectId: 'verticalizados-pg',
-  storageBucket: 'verticalizados-pg.firebasestorage.app',
-  messagingSenderId: '10611554620',
-  appId: '1:10611554620:web:a9efb1984120d042bb4639',
-  measurementId: 'G-LH1QTWJ8BE',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
